c207652c32065e57e54fa1113cde5d36f409cfbb
PaulMbutu <pmbutum24@gmail.com>
2026-05-04 22:36:21 +0300

    added frontend url

--- ecommerceApiProject/settings.py
+++ ecommerceApiProject/settings.py
@@ -66,9 +66,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
 "http://127.0.0.1:8008",
-"http://localhost:3001",
-"http://localhost:3002",
-"https://next-shop-self.vercel.app"
+"https://digitalshop-frontend.vercel.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
